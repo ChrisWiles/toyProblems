73932c536880898f05f7aee7bcb59a25920edcac
Christopher Wiles <chriswiles87@gmail.com>
2016-08-28 10:41:16 -0500

    add solution

---
+++ BubbleSortV.2/BubbleSortV.2.js
@@ -0,0 +1,10 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // es6 destructuring
+      }
+    }
+  }
+  return arr;
+}
